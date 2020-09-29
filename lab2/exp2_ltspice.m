32dd7465617536101997fdf3e35d49005e752c4c
Jack Greenberg <j@jackgreenberg.co>
2020-09-28 22:05:19 -0700

    FIX: final plots for exp2

--- lab2/exp2_ltspice.m
+++ lab2/exp2_ltspice.m
@@ -13,27 +13,40 @@ plot(v_in, VdIV(3,:), "b.", "DisplayName", "R_3 = 10k\Omega", "MarkerSize", 10);
 
 xlabel("Voltage In (V)");
 ylabel("Voltage across Diode (V)")
-legend("FontSize", 12); grid on; hold off;
+legend("FontSize", 12, "Location", "Northwest"); grid on; hold off;
 saveas(fig, "docs/images/exp2-VdIV.eps");
 
 
 fig2 = figure;
 
-semilogy(v_in, IinIV, "r.", "DisplayName", "I_{in} (A)"); hold on;
+semilogy(v_in, IinIV(1,:), "r.", "DisplayName", "R_1 = 100\Omega", "MarkerSize", 10); hold on;
+semilogy(v_in, IinIV(2,:), "g.", "DisplayName", "R_2 = 1k\Omega", "MarkerSize", 10);
+semilogy(v_in, IinIV(3,:), "b.", "DisplayName", "R_3 = 10k\Omega", "MarkerSize", 10);
 
 xlabel("Voltage In (V)");
 ylabel("Current into circuit (A)")
-legend("FontSize", 12); grid on; hold off;
+legend("FontSize", 12, "Location", "Northwest"); grid on; hold off;
 saveas(fig2, "docs/images/exp2-IinIV.eps");
 
 
 fig3 = figure;
 
-plot(v_in, IdIV(1,:), "r.", "DisplayName", "R_1 = 100\Omega", "MarkerSize", 10); hold on;
-plot(v_in, IdIV(2,:), "g.", "DisplayName", "R_2 = 1k\Omega", "MarkerSize", 10);
-plot(v_in, IdIV(3,:), "b.", "DisplayName", "R_3 = 10k\Omega", "MarkerSize", 10);
+subplot(3, 1, 1);
+plot(v_in, 1e3.*IdIV(1,:), "r.", "DisplayName", "R_1 = 100\Omega", "MarkerSize", 10); hold on;
+xlabel("V_{in} (V), R=100\Omega");
+ylabel("I_{in} (mA)");
+ylim([-2.5 25]);
 
-xlabel("Voltage In (V)");
-ylabel("Current across Diode (I)")
-legend("FontSize", 12); grid on; hold off;
+subplot(3, 1, 2);
+plot(v_in, 1e3.*IdIV(2,:), "g.", "DisplayName", "R_2 = 1k\Omega", "MarkerSize", 10);
+xlabel("V_{in} (V), R=1k\Omega");
+ylabel("I_{in} (mA)");
+ylim([-.25 2.5]);
+
+subplot(3, 1, 3);
+plot(v_in, 1e3.*IdIV(3,:), "b.", "DisplayName", "R_3 = 10k\Omega", "MarkerSize", 10);
+xlabel("V_{in} (V), R=10k\Omega");
+ylabel("I_{in} (mA)");
+ylim([-.025 .25]);
+
 saveas(fig3, "docs/images/exp2-IdIV.eps");
